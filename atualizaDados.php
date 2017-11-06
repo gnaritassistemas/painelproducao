aa664d3d0d1f7da8dab502d2bc4d55e048706ebb
gnaritassistemas <33077177+gnaritassistemas@users.noreply.github.com>
2017-11-06 20:55:41 -0300

    atualização 06-11-2017
    atualização 06-11-2017

--- atualizaDados.php
+++ atualizaDados.php
@@ -5,15 +5,15 @@ $db = new SQLite3('/usr/share/pythonProjects/cadeado.db');
 $results = $db->query('SELECT * FROM tab1;');
 $resultadoconsultaRows = $results->fetchArray(1);
 
-    $linha              =       $resultadoconsultaRows['linha'];			
-    $linhaDesc          =       $resultadoconsultaRows['descricao_linha'];			
+    $linha              =   $resultadoconsultaRows['linha'];			
+    $linhaDesc          =   $resultadoconsultaRows['descricao_linha'];			
     $contador           =	$resultadoconsultaRows['contador'];			
     $meta_total         =	$resultadoconsultaRows['meta'];			
     $tempo_ciclo        =	$resultadoconsultaRows['tempo_ciclo'];			
-    $hora_inicio	=	$resultadoconsultaRows['hora_inicio'];		
-    $minuto_inicio	=	$resultadoconsultaRows['minuto_inicio'];		
-    $hora_termino	=	$resultadoconsultaRows['hora_termino'];		
-    $minuto_termino	=	$resultadoconsultaRows['minuto_termino'];		
+    $hora_inicio		=	$resultadoconsultaRows['hora_inicio'];		
+    $minuto_inicio		=	$resultadoconsultaRows['minuto_inicio'];		
+    $hora_termino		=	$resultadoconsultaRows['hora_termino'];		
+    $minuto_termino		=	$resultadoconsultaRows['minuto_termino'];		
    
     $data           = date('H:i');
     $hora_atual     = date('H');
@@ -37,10 +37,11 @@ $resultadoconsultaRows = $results->fetchArray(1);
 
 $resultadoconsultaRows["dataAtual"] = $data;
 $resultadoconsultaRows["no_homens"] = $no_homens;
-$resultadoconsultaRows["realizado"] = '<h1 style="color:'.$cor.'">'.$realizado.'%</h1>';
-'<div class="easypiechart chart-'.$cor.'" id="easypiechart-'.$cor.'" data-percent="'.$realizado.'" style="height: 111px;" >
+$resultadoconsultaRows["realizado"] = '<h1 style="color:'.$cor.'">'.$realizado.'%</h1>
+<div class="easypiechart chart-'.$cor.'" id="easypiechart-'.$cor.'" data-percent="'.$realizado.'" style="height: 111px;" >
     <span class="percent">'.$realizado.'%</span>
 </div>';
 
 
 echo json_encode($resultadoconsultaRows);
+?>
